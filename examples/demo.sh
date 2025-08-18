df03a6ba1949fb773b97993ba4880b0978fd0726
hirokuma <hiro9ma@gmail.com>
2025-08-18 23:22:27 +0900

    examples

--- examples/demo.sh
+++ examples/demo.sh
@@ -1,34 +1,40 @@
 #!/usr/bin/env bash
 
-set -x
+set -xe
 
-cargo build --workspace --all-targets --all-features || exit 1
+cargo build --workspace --all-targets
 export RUST_BACKTRACE=1
 RGB="./target/debug/rgb -d examples/data"
 RGB_2="./target/debug/rgb -d examples/data2"
 
-$RGB init 2>/dev/null
-$RGB_2 init 2>/dev/null
+rm -f \
+    examples/data/bitcoin.testnet/*.issuer \
+    examples/DemoToken.rgb \
+    examples/transfer.psbt \
+    examples/transfer.rgb
+rm -rf \
+    examples/data/bitcoin.testnet/DemoToken.*.contract \
+    examples/data2/bitcoin.testnet/DemoToken.*.contract
 
+$RGB init
+$RGB_2 init
+
+# RGB20-FNA
+# https://github.com/pandora-prime/rgb-issuers/blob/49b4048cdffb93fd84161713891d3d8fa02f7eaf/compiled/RGB20-Simplest-v0-AYkSrg.issuer
 $RGB import examples/RGB20-FNA.issuer
-
-rm -rf examples/data/bitcoin.testnet/DemoToken.*.contract
-rm -rf examples/data2/bitcoin.testnet/DemoToken.*.contract
 $RGB issue -w alice examples/DemoToken.yaml
-$RGB backup -f DemoToken examples/DemoToken.rgb
+$RGB backup DemoToken examples/DemoToken.rgb
 $RGB_2 accept -w bob -u examples/DemoToken.rgb
 
 $RGB contracts
 $RGB state -go -w alice
+$RGB_2 contracts
 $RGB_2 state -go -w bob
-#$RGB fund alice
+
 AUTH_TOKEN=$($RGB_2 invoice -w bob --nonce 0 --seal-only DemoToken)
 INVOICE=$($RGB_2 invoice -w bob --nonce 0 DemoToken 10)
+$RGB pay -w alice "$INVOICE" examples/transfer.rgb examples/transfer.psbt --esplora
+$RGB_2 accept -w bob examples/transfer.rgb
 
-rm examples/transfer.psbt examples/Transfer.yaml examples/transfer.rgb 2>/dev/null
-$RGB pay -w alice "$INVOICE" examples/transfer.rgb examples/transfer.psbt || exit 1
 $RGB state -goa -w alice
-
-$RGB_2 accept -w bob examples/transfer.rgb || exit 1
-
 $RGB_2 state -go -w bob
--- examples/demo_custom.sh
+++ examples/demo_custom.sh
@@ -1,33 +1,45 @@
 #!/usr/bin/env bash
 
-set -x
+set -xe
 
-cargo build --workspace --all-targets --all-features || exit 1
+cargo build --workspace --all-targets
 export RUST_BACKTRACE=1
 RGB="./target/debug/rgb -d examples/data"
 RGB_2="./target/debug/rgb -d examples/data2"
 
-rm -rf examples/data/bitcoin.testnet/DemoToken.contract
-rm -rf examples/data2/bitcoin.testnet/DemoToken.contract
+rm -f \
+    examples/data/bitcoin.testnet/*.issuer \
+    examples/DemoToken.rgb \
+    examples/transfer.psbt \
+    examples/transfer.rgb \
+    examples/transfer.pfab \
+    examples/Transfer.yaml
+rm -rf \
+    examples/data/bitcoin.testnet/DemoToken.*.contract \
+    examples/data2/bitcoin.testnet/DemoToken.*.contract
+
+$RGB init
+$RGB_2 init
+
+# RGB20-FNA
+# https://github.com/pandora-prime/rgb-issuers/blob/49b4048cdffb93fd84161713891d3d8fa02f7eaf/compiled/RGB20-Simplest-v0-AYkSrg.issuer
+$RGB import examples/RGB20-FNA.issuer
 $RGB issue -w alice examples/DemoToken.yaml
-cp -r examples/data/bitcoin.testnet/DemoToken.contract examples/data2/bitcoin.testnet/
+$RGB backup DemoToken examples/DemoToken.rgb
+$RGB_2 accept -w bob -u examples/DemoToken.rgb
 
 $RGB contracts
 $RGB state -go -w alice
-#$RGB fund alice
+$RGB_2 contracts
+$RGB_2 state -go -w bob
+
 AUTH_TOKEN=$($RGB_2 invoice -w bob --nonce 0 --seal-only DemoToken)
 INVOICE=$($RGB_2 invoice -w bob --nonce 0 DemoToken 10)
+$RGB script -w alice "$INVOICE" examples/Transfer.yaml --esplora
+$RGB exec -w alice examples/Transfer.yaml examples/transfer.pfab 1000 examples/transfer.psbt
+$RGB complete -w alice examples/transfer.pfab examples/transfer.psbt
+$RGB consign DemoToken -t "$AUTH_TOKEN" examples/transfer.rgb
+$RGB_2 accept -w bob examples/transfer.rgb
 
-rm examples/transfer.psbt examples/Transfer.yaml
-$RGB script -w alice "$INVOICE" examples/Transfer.yaml || exit 1
-$RGB exec -w alice examples/Transfer.yaml examples/transfer.pfab 1000 examples/transfer.psbt || exit 1
-
-$RGB complete -w alice examples/transfer.pfab examples/transfer.psbt || exit 1
-
-rm examples/transfer.rgb
-$RGB consign DemoToken -t "$AUTH_TOKEN" examples/transfer.rgb || exit 1
 $RGB state -goa -w alice --sync --mempool
-
-$RGB_2 accept -w bob examples/transfer.rgb || exit 1
-
 $RGB_2 state -go -w bob --sync --mempool
